7b4107ce681216d729f33a041923fcf5a142b00f
KauaO <kaua.o@escolar.ifrn.edu.br>
2022-11-08 10:07:58 -0300

    model commit

--- main/models.py
+++ main/models.py
@@ -6,8 +6,21 @@ LISTA_SEXO = [
     ('Feminino','Feminino'),
 ]
 
+LISTA_CURSO = [
+    ('Curso Técnico','Curso Técnico'),
+    ('Curso Integrado','Curso Integrado'),
+]
+    
+    
+
 class Aluno(models.Model):
     nome = models.CharField(max_length=150)
+    cpf = models.CharField(max_length=50)
     email = models.EmailField()
     data_nascimento = models.DateField()
-    sexo = models.CharField(max_)
+    endereco = models.CharField(max_length=150)
+    sexo = models.CharField(max_length=150, choices=LISTA_SEXO)
+    curso = models.CharField(max_length=150, choices=LISTA_CURSO) 
+
+    def __str__(self):
+        return self.nome
