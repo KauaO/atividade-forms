6a10715eaf012708cace74e9dd3ba3a5b81e3e46
KauaO <kaua.o@escolar.ifrn.edu.br>
2022-11-22 10:24:37 -0300

    cummit admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from main.models import *
+from main.forms import *
 # Register your models here.
+
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = { 'nome', 'email', 'sexo', 'curso', 'minicurso'}
+    search_fields = ('nome')
+    list_filter = {'curso'}
+
+
+   
+
+
+
+admin.site.register(AlunoAdmin, Aluno)
